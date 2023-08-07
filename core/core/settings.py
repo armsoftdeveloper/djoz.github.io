664efe8d53bd99ac8fef49b20d72f5ae58fa492e
Armen Vardanyan <91540963+armsoftdeveloper@users.noreply.github.com>
2023-08-07 17:27:42 +0400

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -212,10 +212,10 @@ CKEDITOR_CONFIGS = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'arm.vardanyan9559@gmail.com'
-EMAIL_HOST_PASSWORD = 'rrhiiozmeutkmkhu'
+EMAIL_HOST_USER = 'your-email'
+EMAIL_HOST_PASSWORD = '#############'
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'arm.vardanyan9559@gmail.com'
+DEFAULT_FROM_EMAIL = 'your-email'
 
 # Internal IPS For Django Debug Toolbar
 
